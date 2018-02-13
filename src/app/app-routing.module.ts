2849fa2f2d3b62ad890fdc150d922fbc856ef80a
Lucas Nascimento <csalucasnascimento@gmail.com>
2018-02-13 17:31:09 +1100

    styles according to sass references

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,31 @@
 import { NgModule } from '@angular/core';
-import { appRoutes } from './app-routes'
 import { RouterModule }  from '@angular/router';
+import { Routes } from '@angular/router';
+
+import {
+  SignInComponent,
+  SignUpComponent,
+  LayoutComponent,
+  ForgotPasswordComponent
+} from '@shared/components'
+
+import {
+  UserListComponent
+} from '@features/users'
+
+const appRoutes: Routes = [
+  { path: 'sign-in', component: SignInComponent },
+  { path: 'register', component: SignUpComponent },
+  { path: '',
+    component: LayoutComponent,
+    // canActivate: [AuthGuard, AuthGuardVerified],
+    children: [
+      { path: 'users', component: UserListComponent }
+    ] 
+  },
+  { path: '**', redirectTo: '/page-not-found', pathMatch: 'full' },
+  // { path: 'page-not-found', component:  }
+]
 
 @NgModule({
   imports: [
