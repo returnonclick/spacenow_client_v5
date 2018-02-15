926202e2afa9ecbb794250b48e6c23f99bafc73a
Lucas Nascimento <csalucasnascimento@gmail.com>
2018-02-15 17:36:26 +1100

    home and card component

--- src/app/features/pages/home/home.component.ts
+++ src/app/features/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core'
+import { CardComponent } from '@shared/components/custom/card/card.component'
 
 @Component({
   selector: 'sn-home',
@@ -6,5 +7,23 @@ import { Component } from '@angular/core'
   styleUrls: [ './home.component.scss' ]
 })
 export class HomeComponent {
-  
+
+  sliderComponent: any = CardComponent
+  data: Array<any> = [{
+    isNew: true
+  }, {
+    isNew: !true
+  }, {
+    isNew: true
+  }, {
+    isNew: true
+  }, {
+    isNew: !true
+  }, {
+    isNew: true
+  }, {
+    isNew: !true
+  }, {
+    isNew: true
+  }]
 }
--- src/app/shared/components/custom/index.ts
+++ src/app/shared/components/custom/index.ts
@@ -1,31 +1,37 @@
-import { ConfirmDeleteComponent } from "./confirm-delete/confirm-delete.component";
-import { ConfirmSaveComponent } from "./confirm-save/confirm-save.component";
+import { CardComponent } from "./card/card.component"
+import { ConfirmDeleteComponent } from "./confirm-delete/confirm-delete.component"
+import { ConfirmSaveComponent } from "./confirm-save/confirm-save.component"
 import { FilterComponent } from './filter/filter.component'
 import { GoogleAddressComponent } from './google-address/google-address.component'
-import { ImageDataComponent } from "./image-data/image-data.component";
+import { ImageDataComponent } from "./image-data/image-data.component"
 import { InputCardComponent } from './input-card/input-card.component'
+import { SliderComponent } from './slider/slider.component'
 import { TableComponent } from './table/table.component'
 import { TableSelectorComponent } from './table-sel/table-sel.component'
 
 
 export {
+  CardComponent,
   ConfirmDeleteComponent,
   ConfirmSaveComponent,
   FilterComponent,
   GoogleAddressComponent,
   ImageDataComponent,
   InputCardComponent,
+  SliderComponent,
   TableComponent,
   TableSelectorComponent
 }
 
 export default {
+  CardComponent,
   ConfirmDeleteComponent,
   ConfirmSaveComponent,
   FilterComponent,
   GoogleAddressComponent,
   ImageDataComponent,
   InputCardComponent,
+  SliderComponent,
   TableComponent,
   TableSelectorComponent
 }
