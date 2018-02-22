4e1fa46a05877e1acd6c0f9725c5a4843acced69
Lucas Nascimento <csalucasnascimento@gmail.com>
2018-02-23 08:32:11 +1100

    authentication with Facebook and Linkedin

--- src/app/core/store/auth/actions/auth.ts
+++ src/app/core/store/auth/actions/auth.ts
@@ -4,6 +4,7 @@ import { User } from '@shared/models/user';
 
 export const GET_USER = '[Auth] Get User';
 export const SIGN_IN = '[Auth] SignIn';
+export const SIGN_UP = '[Auth] SignUp';
 export const SIGN_IN_WITH_PROVIDER = '[Auth] SignIn with provider';
 export const SIGN_OUT = '[Auth] SignOut';
 export const SUCCESS = '[Auth] Sucess';
@@ -17,6 +18,12 @@ export class GetUser implements Action {
 
 export class SignIn implements Action {
     public readonly type = SIGN_IN;
+
+    constructor(public payload: {username: string, password: string}) {}
+}
+
+export class SignUp implements Action {
+    public readonly type = SIGN_UP;
 
     constructor(public payload: {username: string, password: string}) {}
 }
