974d80db0d9b9e9e00c6d0c67603401012feadc9
Errol Pineda <rrlqzdpnd@gmail.com>
2018-03-06 12:58:11 +1100

    code refactor

--- src/app/features/braintree/components/braintree-ui.component.ts
+++ src/app/features/braintree/components/braintree-ui.component.ts
@@ -15,27 +15,26 @@ export class BraintreeUIComponent {
   // paymentServerDomain = 'http://localhost:8080/api/'
   paymentServerDomain = "https://glacial-ocean-69657.herokuapp.com/api/"
 
-  @Input() clientTokenURL: string
-  @Input() requestType:    string
-  @Input() requestData:    any
+  @Input() clientTokenURL:     string
+  @Input() requestType:        string
+  @Input() requestData:        any
   // Optional inputs
-  @Input() buttonText:         string  = 'Update Card' // to configure the pay button text
-  @Input() allowChoose:        boolean = false
-  @Input() showCardholderName: boolean = false
-  @Input() showPaypal:         boolean = false
-  @Input() customerId:         string  = ""
+  @Input() buttonText:         string            = 'Update Card' // to configure the pay button text
+  @Input() allowChoose:        boolean           = false
+  @Input() showCardholderName: boolean           = false
+  @Input() showPaypal:         boolean           = false
+  @Input() customerId:         string            = ""
 
-  @Output() outcomeStatus: EventEmitter<any> = new EventEmitter<any>()
+  @Output() outcomeStatus:     EventEmitter<any> = new EventEmitter<any>()
 
-  clientToken: string
-  nonce: string
-  showDropinUI = true
-  errorMessage: string
-
-  showPayButton = false // to display the pay button only after the dropin UI has rendered (well, almost)
-  clientTokenNotReceived = false // to show the error, "Error! Client token not received."
-  interval: any
-  instance: any
+  initDropIn:                  any
+  dropInInstance:              any
+  clientToken:                 string
+  nonce:                       string
+  showPayButton:               boolean           = false // to display the pay button only after the dropin UI has rendered (well, almost)
+  showDropInUI:                boolean           = true
+  clientTokenNotReceived:      boolean           = false // to show the error, "Error! Client token not received."
+  errorMessage:                string
 
   constructor(private service: BraintreeService) { }
 
@@ -47,121 +46,125 @@ export class BraintreeUIComponent {
     this.service
       .getClientToken(this.clientTokenURL, this.customerId)
       .subscribe((clientToken: string) => {
-        if (!clientToken) {
+        if(!clientToken)
           this.clientTokenNotReceived = true
-        } else {
-          this.clientToken = clientToken
+        else {
+          this.clientToken            = clientToken
           this.clientTokenNotReceived = false
-          this.interval = setInterval(() => { this.createDropin() }, 0)
+          this.initDropIn             = setInterval(() => {
+            this.createDropin()
+          }, 0)
         }
       }, (error) => {
         this.clientTokenNotReceived = true
-        console.error(`Client token not received.
-        Please make sure your braintree server api is configured properly, running and accessible.`)
+        console.error(`Client token not received. Please make sure your braintree server api is configured properly, running and accessible.`)
       })
   }
 
   createDropin() {
-    var dropinConfig: any = {}
-
-    dropinConfig.authorization = this.clientToken
-    dropinConfig.container = '#dropin-container'
+    let config: any = {
+      authorization: this.clientToken,
+      container:     '#dropin-container'
+    }
 
     // showPaypal
     if(this.showPaypal) {
-      dropinConfig.paypal = {
+      config.paypal = {
         flow: 'vault'
       }
     }
     // showCardholderName
-    if (this.showCardholderName) {
-      dropinConfig.card = {
+    if(this.showCardholderName) {
+      config.card = {
         cardholderName: {
           required: this.showCardholderName
         }
       }
     }
 
-    dropin.create(dropinConfig, (createErr, instance) => {
-      if (createErr) {
+    dropin.create(config, (createErr, dropInInstance) => {
+      if(createErr) {
         console.error(createErr)
         return
       }
-      this.instance = instance
+      this.dropInInstance = dropInInstance
 
       // Check whether a saved credit card is avablable
-      console.log(this.instance.isPaymentMethodRequestable())
+      console.log(this.dropInInstance.isPaymentMethodRequestable())
 
       // Listen to Dropin UI event
-      instance.on('paymentMethodRequestable', function(event){
+      dropInInstance.on('paymentMethodRequestable', function(event) {
         console.log(event.type)
         console.log(event.paymentMethodIsSelected)
       })
-
     })
-    clearInterval(this.interval)
+    clearInterval(this.initDropIn)
     this.showPayButton = true
-
-
   }
 
   update(): void {
-    if (this.instance) {
-      this.instance.requestPaymentMethod((err, payload) => {
-        if (err) {
-          console.error(err)
-          return
-        }
-        if (!this.allowChoose) { // process immediately after tokenization
-          this.nonce = payload.nonce
-          this.showDropinUI = false
-          this.confirmUpdate()
-        } else if (!this.nonce) { // dont process immediately. Give user a chance to change his payment details.
-          this.nonce = payload.nonce // previous nonce doesn't exist
-        } else if (this.nonce === payload.nonce) { // a nonce exists already
-          this.confirmUpdate() // go ahead with payment
-        } else {
-          this.nonce = payload.nonce
-        }
-      })
+    if(!this.dropInInstance) {
+      return
     }
+
+    this.dropInInstance.requestPaymentMethod((err, payload) => {
+      if(err) {
+        console.error(err)
+        return
+      }
+
+      if(!this.allowChoose) { // process immediately after tokenization
+        this.nonce        = payload.nonce
+        this.showDropInUI = false
+        this.confirmUpdate()
+      }
+      else if(!this.nonce) // dont process immediately. Give user a chance to change his payment details.
+        this.nonce = payload.nonce // previous nonce doesn't exist
+      else if(this.nonce === payload.nonce) // a nonce exists already
+        this.confirmUpdate() // go ahead with payment
+      else
+        this.nonce = payload.nonce
+    })
   }
 
   confirmUpdate(): void {
-    this.showDropinUI = false
-    const requestURL = this.paymentServerDomain + this.requestType
-    console.log(this.requestType)
-    switch (this.requestType) {
+    this.showDropInUI = false
+    const requestURL  = this.paymentServerDomain + this.requestType
+
+    switch(this.requestType) {
       case 'booking-payment':
         console.log("paying for a booking")
-        this.showDropinUI = false
-        this.service
-          .payBooking(requestURL, this.nonce, this.requestData)
-          .subscribe((status: any) => {
-            if (status.errors) {
-              this.errorMessage = status.message
-              this.showDropinUI = true
-              this.generateDropInUI()
-            }
-            this.outcomeStatus.emit(status)
+        this.service.payBooking(
+          requestURL,
+          this.nonce,
+          this.requestData
+        ).subscribe(status => {
+          if(status.errors) {
+            this.errorMessage = status.message
+            this.showDropInUI = true
+            this.generateDropInUI()
+          }
+
+          this.outcomeStatus.emit(status)
         })
         break
       case 'new-payment-method':
-        this.showDropinUI = false
-        this.service
-          .createPaymentMethod(requestURL, this.nonce, this.requestData)
-          .subscribe((status: any) => {
-            if (status.errors) {
-              this.errorMessage = status.message
-              this.showDropinUI = true
-              this.generateDropInUI()
-            }
+        this.service.createPaymentMethod(
+          requestURL,
+          this.nonce,
+          this.requestData
+        ).subscribe(status => {
+          if(status.errors) {
+            this.errorMessage = status.message
+            this.showDropInUI = true
+            this.generateDropInUI()
+          }
 
-            this.outcomeStatus.emit(status)
-          })
+          this.outcomeStatus.emit(status)
+        })
         break
       default:
-        this.showDropinUI = true
+        this.showDropInUI = true
         break
     }
   }
