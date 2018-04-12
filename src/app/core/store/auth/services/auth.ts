1351433863df86c56efae0fa8ba83d8347fec3b1
Errol Pineda <rrlqzdpnd@gmail.com>
2018-04-12 16:12:37 +1000

    code clean-up and log out issue and recover password issues fixed

--- src/app/core/store/auth/services/auth.ts
+++ src/app/core/store/auth/services/auth.ts
@@ -1,45 +1,33 @@
 import { Injectable } from '@angular/core'
-import { Router } from '@angular/router'
-import { of } from 'rxjs/observable/of'
-import * as firebase from 'firebase/app'
+import { Store } from '@ngrx/store'
 import { AngularFireAuth } from 'angularfire2/auth'
-import { AngularFirestore, AngularFirestoreDocument, AngularFirestoreCollection } from 'angularfire2/firestore'
+import { AngularFirestore } from 'angularfire2/firestore'
 
-import { Observable } from 'rxjs/Observable'
-import 'rxjs/add/operator/switchMap'
+import * as firebase from 'firebase/app'
 
-import { Store, State } from '@ngrx/store'
-import * as actions from '@core/store/auth/actions/auth'
+import { User } from '@models/user'
+import { Profile } from '@models/profile'
+import { Contact } from '@models/contact'
 
 import * as fromRoot from '@core/store'
-import { User } from '@shared/models/user'
-import { UserData } from '@shared/models/user-data'
-import { Profile } from '@shared/models/profile'
-import { Contact } from '@shared/models/contact'
+import * as actions from '@core/store/auth/actions/auth'
 
 @Injectable()
 export class AuthService {
 
-  user: Observable<User | null>;
-
   constructor(
     private _store: Store<fromRoot.State>,
-    public afAuth: AngularFireAuth,
-    private _afs: AngularFirestore,
-    private router: Router
+    public afAuth:  AngularFireAuth,
+    private _afs:   AngularFirestore,
   ) {
-
-    this.afAuth.authState.subscribe(
-      (user) => {
-        if (user)
-          this._store.dispatch(new actions.GetUser)
-      }
-    )
-    
+    this.afAuth.authState.subscribe(user => {
+      if(user)
+        this._store.dispatch(new actions.GetUser)
+    })
   }
 
   getUser(id) {
-     return this._afs.collection(`users`, ref => ref.where(`uid`, "==", `${id}`)).stateChanges()
+    return this._afs.doc(`/users/${id}`).ref.get()
   }
 
   signIn(username, password) {
@@ -47,15 +35,13 @@ export class AuthService {
   }
 
   signInWithProvider(provider) {
-    return this.afAuth.auth.signInWithPopup(provider).then(
-      data => this.updateUserData(data.user)
-    )
+    return this.afAuth.auth.signInWithPopup(provider)
+      .then(data => this.updateUserData(data.user))
   }
 
   signUp(username, password) {
-    return this.afAuth.auth.createUserWithEmailAndPassword(username, password).then(
-      data =>  this.updateUserData(data) 
-    )
+    return this.afAuth.auth.createUserWithEmailAndPassword(username, password)
+      .then(data => this.updateUserData(data))
   }
 
   sendEmailVerification() {
@@ -72,19 +58,16 @@ export class AuthService {
   }
 
   private updateUserData(auth: any, credential?: any) {
-
-    const userRef: AngularFirestoreDocument<User> = this._afs.collection<User>(`users`).doc(`${auth.uid}`)
-    const userProfileRef: AngularFirestoreDocument<Profile> = this._afs.collection<Profile>(`users-profile`).doc(`${auth.uid}`)
-     
-    const data: User = new User(auth)
-    const contact: Contact = new Contact()
-    let userProfile: Profile = new Profile()
-    userProfile.uid = data.uid
+    const userRef        = this._afs.collection<User>(`users`).doc(`${auth.uid}`)
+    const userProfileRef = this._afs.collection<Profile>(`users-profile`).doc(`${auth.uid}`)
+    const data          = new User(auth)
+    const contact       = new Contact()
+    let userProfile     = new Profile()
+    userProfile.uid     = data.uid
     userProfile.contact = contact
 
-    userRef.set(Object.assign({}, data))
-    userProfileRef.set(Object.assign({}, userProfile))
-
+    userRef.set(Object.assign({ }, data))
+    userProfileRef.set(Object.assign({ }, userProfile))
   }
 
 }
