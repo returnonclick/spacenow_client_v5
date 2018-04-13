b4dd0b6828bb3009b599de580f13bc659586a8cc
Errol Pineda <rrlqzdpnd@gmail.com>
2018-04-13 11:38:39 +1000

    about page overhaul

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core'
+import { Store } from '@ngrx/store'
+
+import * as fromRoot from '@core/store'
+import * as layoutActions from '@core/store/layouts/actions/layout'
 
 @Component({
   selector: 'sn-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss']
 })
-
 export class AboutComponent {
- 
+
+  constructor(private _store: Store<fromRoot.State>) { }
+
+  ngOnInit() {
+    this._store.dispatch(new layoutActions.SetLogoWhite)
+  }
+
 }
- 
