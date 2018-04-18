2151c8fb5c6ccdb3def9c2476e68dac471e7809f
sugarme <sgthangtran@gmail.com>
2018-04-18 16:05:48 +1000

    feat(deployment): setup for firebase deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,12 @@
 export const environment = {
-    production: true,
-    firebase: {
-      apiKey: "api key",
-      authDomain: "auth domain",
-      databaseURL: "database url",
-      projectId: "project id",
-      storageBucket: "storage bucket",
-      messagingSenderId: "messaging sender id"
-    },
-    googleApi: "AIzaSyAOMhtcFm3WnFDUAsK7OfWOvpo3V0EtvBQ",
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDPThaUFMtL1-hWllScsDsKuu002DrRqIU",
+    authDomain: "spacenow-bca9c.firebaseapp.com",
+    databaseURL: "https://spacenow-bca9c.firebaseio.com",
+    projectId: "spacenow-bca9c",
+    storageBucket: "spacenow-bca9c.appspot.com",
+    messagingSenderId: "259396936923"
+  },
+  googleApi: "AIzaSyAOMhtcFm3WnFDUAsK7OfWOvpo3V0EtvBQ",
 };
